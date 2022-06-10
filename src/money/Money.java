fb89f9ce1d7ec1aea6f9b4cee38ce662a6cafe10
noaki-k <knk43nok@gmail.com>
2022-06-11 07:48:19 +0900

    chapter10: テストに聞いてみる

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,9 +1,11 @@
 package money;
 
-abstract class Money {
+class Money {
     protected int amount;
     protected String currency;
-    abstract Money times(int multiplier);
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
     Money(int amount, String currency) {
         this.amount = amount;
@@ -12,7 +14,7 @@ abstract class Money {
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency.equals(money.currency);
     }
 
     static Money dollar(int amount) {
