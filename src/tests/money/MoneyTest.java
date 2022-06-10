e2b0e49a02a9a5aa6d2f936fd7a34fdc150e43ab
noaki-k <knk43nok@gmail.com>
2022-06-11 07:53:51 +0900

    chapter11: 不要になったら消す

--- src/tests/money/MoneyTest.java
+++ src/tests/money/MoneyTest.java
@@ -5,18 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
     @Test
-    public void testMultiplication() {
-        Money five = Money.dollar(5);
-        assertEquals(Money.dollar(10), five.times(2));
-        assertEquals(Money.dollar(15), five.times(3));
-    }
-
-    @Test
     public void testEquality(){
         assertEquals(Money.dollar(5), Money.dollar((5)));
         assertNotEquals(Money.dollar(5), Money.dollar(6));
-        assertEquals(Money.franc(5), Money.franc((5)));
-        assertNotEquals(Money.franc(5), Money.franc(6));
         assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
