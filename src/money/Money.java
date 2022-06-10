ffa8692dd63f6fc8af220de3ec91e5c662f03141
noaki-k <knk43nok@gmail.com>
2022-06-10 09:08:15 +0900

    chapter6: テスト不足に気づいたら
    まずはテストを追加する。その後リファクタリングに着手する

---
+++ src/money/Money.java
@@ -0,0 +1,10 @@
+package money;
+
+public class Money {
+    protected int amount;
+
+    public boolean equals(Object object) {
+        Money dollar = (Money) object;
+        return amount == dollar.amount;
+    }
+}
